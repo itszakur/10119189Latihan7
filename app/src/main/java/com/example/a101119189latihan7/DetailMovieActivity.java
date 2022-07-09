d6cf05bb9bac630f334dd4b40afa9d6ee604b28c
Reza Kurnia <ezakurnia50@gmail.com>
2022-07-09 17:13:21 +0700

    10119189Latihan7

---
+++ app/src/main/java/com/example/a101119189latihan7/DetailMovieActivity.java
@@ -0,0 +1,14 @@
+package com.example.a101119189latihan7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
